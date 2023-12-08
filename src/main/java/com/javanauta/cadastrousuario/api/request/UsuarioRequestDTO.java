688322d988f391ac682b22b9793df3cafcc90e35
Tiago Bezerra <tnthiagolima88@gmail.com>
2023-12-08 14:49:44 -0300

    Microserviços em Java. Com uso de BFF

---
+++ src/main/java/com/javanauta/cadastrousuario/api/request/UsuarioRequestDTO.java
@@ -0,0 +1,22 @@
+package com.javanauta.cadastrousuario.api.request;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioRequestDTO {
+
+    private String nome;
+
+    private String email;
+
+    private String documento;
+
+    private EnderecoRequestDTO endereco;
+
+
+}
